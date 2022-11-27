d095d19bfd415f7d0b61b90593448ed91241e6a8
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2022-11-27 19:17:25 +0100

    first

---
+++ src/GUI/controller/MyTunesController.java
@@ -0,0 +1,24 @@
+package GUI.controller;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyTunesController implements Initializable {
+
+    public ProgressBar progressBar;
+    public Label txtNowPlaying;
+    public Slider volumeSlider;
+    public TextField searchBar;
+    public Button btnPlay;
+    public Button btnPause;
+    public Button btnPrevious;
+    public Button btnNext;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/MyTunes.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class MyTunes extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GUI/view/MyTunes.fxml")));
+
+        Scene scene = new Scene(root);
+
+
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("MyTunes");
+        stage.centerOnScreen();
+
+
+        stage.show();
+    }
+
+
+    public static void main (String[] args) {
+        launch();
+    }
+}
